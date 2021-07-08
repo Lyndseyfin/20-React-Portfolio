53e240b403437bf2d3eaf0eafcceb09c64bae5b7
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-07-08 08:47:25 -0600

    components

---
+++ src/components/contact.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class AboutMe extends Component {
+
+    render(){
+        return(
+            <div className="div">
+            <h2 className="head">ABOUT ME</h2>
+              <h4 className="h4">
+                  <br></br>
+                  <br></br>
+              <p className="h3">
+              Hey! I'm Lyndsey. Nice to meet you.
+I'm currently finishing up a full stack flex program and am an aspiring front end web developer. 
+I live in Denver, CO with my amazing husband, beautiful daughter and incredibly lazy dog.
+        </p>
+        <p className="h3">
+     Skills:
+Browser Based Technologies (HTML, CSS, JavaScript, jQuery)
+React
+Databases (MySQL, MongoDB)
+Deployment (Heroku, Git, Netlify)
+Server Side Development (Node.js, Express, MERN Stack) 
+    </p>
+</h4>
+</div>
+    )
+  }
+}
+
+export default AboutMe;
