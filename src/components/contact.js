453632504d4d9bf84a004a58a437fb9cb9228f8d
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-07-08 09:00:51 -0600

    aboutme

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,31 +1,56 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
-class AboutMe extends Component {
 
-    render(){
-        return(
-            <div className="div">
-            <h2 className="head">ABOUT ME</h2>
-              <h4 className="h4">
-                  <br></br>
-                  <br></br>
-              <p className="h3">
-              Hey! I'm Lyndsey. Nice to meet you.
-I'm currently finishing up a full stack flex program and am an aspiring front end web developer. 
-I live in Denver, CO with my amazing husband, beautiful daughter and incredibly lazy dog.
-        </p>
-        <p className="h3">
-     Skills:
-Browser Based Technologies (HTML, CSS, JavaScript, jQuery)
-React
-Databases (MySQL, MongoDB)
-Deployment (Heroku, Git, Netlify)
-Server Side Development (Node.js, Express, MERN Stack) 
-    </p>
-</h4>
-</div>
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Lyndsey Finamore</h2>
+            {/* <img
+             I
+              alt="photo"
+              style={{height: '250px'}}
+               />
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p> */}
+
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    720.201.5989
+                  </ListItemContent>
+                </ListItem>
+
+              
+
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    lyndseyfinamore@gmail.com
+                  </ListItemContent>
+                </ListItem>
+
+                <ListItem>
+                 
+                </ListItem>
+
+
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
 
-export default AboutMe;
+export default Contact;
