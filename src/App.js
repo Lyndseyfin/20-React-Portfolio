1ec26cb9c6db023e804d3e77e5019f1f7aaea3d8
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-07-13 08:30:50 -0600

    contact done

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
               <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">LF Portfolio</Link>}>
             <Navigation>
               <Link to="/aboutme" >About Me</Link>
               <Link to="/projects">Projects</Link>
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-  export default function Contact() {
+export default function Contact() {
   return (
-    <div>
-      <h1>Let's Connect</h1>
-      <link href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap" rel="stylesheet"/>
- <Form className="formcontainer">
-  <Form.Group className="form" controlId="exampleForm.ControlInput1">
-    <Form.Label>Name </Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
-  </Form.Group>
-
-  <Form.Group className="form" controlId="exampleForm.ControlInput1">
-    <Form.Label>Email </Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
-  </Form.Group>
- 
-  <Form.Group className="form" controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Message </Form.Label>
-    <Form.Control as="textarea" rows={3} />
-  </Form.Group>
- <Button className="button" variant="dark">Send</Button>{' '}
- </Form>
- </div>
+    <fieldset>
+      <br></br>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Julius+Sans+One&display=swap"
+        rel="stylesheet"
+      />
+      <div className="connect">Let's Connect</div>
+      <Form className="formcontainer">
+        <Form.Group className="form" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name </Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="form" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email </Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="form" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Message </Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button className="button" variant="dark">
+          Send
+        </Button>{" "}
+      </Form>
+    </fieldset>
   );
 }
-
-  
-
 
 // export default Contact;
